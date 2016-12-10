e3b2e1a5fce47bbc2ba73b39de7d1d75481d1baa
Martin Herrmann <martin@famherrmann.de>
2016-12-10 15:06:51 +0100

    Add plotlib unit tests

---
+++ plotlib-test/test-tag.py
@@ -0,0 +1,15 @@
+import unittest
+
+from plotlib import tag
+
+class Test(unittest.TestCase):
+    def testTag(self):
+        tags = tag.make_tags_list(["foo", "bar", "baz"])
+        self.assertEqual(tags, ["foo", "bar", "baz"])
+
+        tags = tag.make_tags_list("qux,quux,quuux")
+        self.assertEqual(tags, ["qux", "quux", "quuux"])
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
