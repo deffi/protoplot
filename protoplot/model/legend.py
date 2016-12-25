f3412b7703d16ade6c7430cd965c8276ee9e5ad6
Martin Herrmann <martin@famherrmann.de>
2016-12-25 22:35:41 +0100

    Add notes on legend location

--- protoplot/model/legend.py
+++ protoplot/model/legend.py
@@ -1,14 +1,26 @@
 from protoplot.engine import Item
 
-#         if 'loc' in opts:
-#             handles, labels = ax.get_legend_handles_labels()
-#             
-#             if 'transform' in opts:
-#                 transform = self.options['transform']
-#                 del self.options['transform']
-#                 handles, labels = transform(handles, labels)
-#                 
-#             ax.legend(handles, labels, **self.options)
+# TODO define the possible locations of the legend - for example:
+#
+#     left-of above    left above    above    right above    right-of above
+#                    .-------------------------------------.
+#     left-of top    | left top      top      right top    | right-of top
+#                    |                                     |
+#     left-of        | left          center   right        | right-of
+#                    |                                     |
+#     left-of bottom | left bottom   bottom   right bottom | right-of bottom
+#                    '-------------------------------------'
+#     left-of below    left below    below    right below    right-of below
+#
+# Additionally:
+#   * "auto" - inside at the corner/side with the most space 
+#   * Order is irrelevant, e. g. "top left" instead of "left top"
+#   * Allow "center" for one direction, e. g. "top center" or "center top"
+#   * Arbitrary position with anchor
+#   * Arbitrary position where 0 is left/top and 1 is right/bottom
+#   * Cardinal directions, e. g. "northwest"/"nw", "northnorthwest"/"nnw"
+#
+# TODO transform function that returns a permutation (list of indices) (?)
 
 class Legend(Item):
     def __init__(self, *args, **kwargs):
