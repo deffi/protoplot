a1c5f6dde24235be0674a24f2c0f0ca57775faeb
Martin Herrmann <martin@famherrmann.de>
2016-12-25 20:30:26 +0100

    Add x/y options to plot

--- protoplot/model/plot.py
+++ protoplot/model/plot.py
@@ -1,14 +1,28 @@
 from protoplot.engine import Item, ItemContainer
+#from protoplot.model import Legend, Series, Text
 from protoplot.model.legend import Legend
 from protoplot.model.series import Series
+from protoplot.model.text import Text
 
 class Plot(Item):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.options.register("backgroundColor", True, "white")
+
+        self.options.register("xlabel", False, "")
+        self.options.register("ylabel", False, "")
+        self.options.register("xlim", False, None)
+        self.options.register("ylim", False, None)
+        self.options.register("xlog", False, None)
+        self.options.register("ylog", False, None)
+        self.options.register("xshift", False, None)
+        self.options.register("yshift", False, None)
+        self.options.register("xticks", False, None)
+        self.options.register("xticks", False, None)
  
         self.series = ItemContainer(Series)
+        self.text   = ItemContainer(Text)
         self.legend = Legend()
 
         # Options for plot:
