abe461b9c3b1f8ed64284cc1b64254eb0b46f9b1
Martin Herrmann <martin@famherrmann.de>
2016-12-25 20:30:36 +0100

    Add text and position

--- protoplot/model/text.py
+++ protoplot/model/text.py
@@ -1,10 +1,13 @@
 from protoplot.engine import Item
 
 class Text(Item):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, position=None, text=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Data: position, text
 
         self.options.register("color"    , True , None)
         self.options.register("anchor"   , False, "center")
+
+        self.position = position
+        self.text = text
